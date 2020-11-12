1067577019b75a0d9249b7a821fe0a51a36c2e7d
propil5 <pilch_piotr@outlook.com>
2020-11-12 22:14:04 +0100

    Added gRPC project.

---
+++ LogisticProcessor/LogisticProcessor.Data.gRPCService/Program.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LogisticProcessor.Data.gRPCService
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+
+        // Additional configuration is required to successfully run gRPC on macOS.
+        // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
