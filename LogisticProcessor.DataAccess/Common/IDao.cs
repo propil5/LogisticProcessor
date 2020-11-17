2c8235f3af8a22f1be8da64c30582515b8e18115
propil5 <pilch_piotr@outlook.com>
2020-11-18 00:43:23 +0100

    Working more with EF Core methods.

---
+++ LogisticProcessor.DataAccess/Common/IDao.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogisticProcessor.DataAccess.Common
+{
+    public interface IDao<TEntity>
+    {
+        IEnumerable<TEntity> Get(int? id = null);
+        TEntity Save(TEntity entity);
+        void Delete(TEntity entity);
+        void SaveChanges();
+    }
+}
---
+++ LogisticProcessor.DataAccess/Dao/AddressDao.cs
@@ -0,0 +1,45 @@
+﻿using LogisticProcessor.DataAccess.Common;
+using LogisticProcessor.DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogisticProcessor.DataAccess.Dao
+{
+    public class AddressDao : IDao<AddressDto>
+    {
+        private ApplicationDbContext _context = null;
+
+        public AddressDao()
+        {
+            ResetContext();
+        }
+
+        private void ResetContext()
+        {
+            _context = new ApplicationDbContext().CreateDbContext(null);
+        }
+
+        public IEnumerable<AddressDto> Get(int? id = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IDao<AddressDto>.Delete(AddressDto entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        AddressDto IDao<AddressDto>.Save(AddressDto entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IDao<AddressDto>.SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
