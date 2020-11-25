2a1cea764194abcc35b6c5bae52fc6049f00a59c
propil5 <pilch_piotr@outlook.com>
2020-11-25 23:55:56 +0100

    Preparing next logic for planning.

---
+++ src/LogisticProcessor.Web/Server/Controllers/Planner/PlanningController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LogisticProcessor.Web.Server.Controllers.Planner
+{
+    public class PlanningController : Controller
+    {
+         //TODO: Add new planning logic that will be used by ML to update this service with new details and als pass that further for displaying that info..
+    }
+}
