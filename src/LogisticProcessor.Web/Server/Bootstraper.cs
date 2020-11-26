e3fb5fa3e6284f19e7dfce8c0fbf5aee1ebe5209
propil5 <pilch_piotr@outlook.com>
2020-11-26 23:54:37 +0100

    Added bootstraper for DI.

---
+++ src/LogisticProcessor.Web/Server/Bootstraper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LogisticProcessor.Web.Server
+{
+    //TODO: Add DI logic here
+    public class Bootstraper
+    {
+    }
+}
