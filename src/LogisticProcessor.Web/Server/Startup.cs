4440a5cedf6915d594ce2328c19a730cca4f64f7
propil5 <pilch_piotr@outlook.com>
2020-11-27 23:55:36 +0100

    Moved DI to another file.

--- src/LogisticProcessor.Web/Server/Startup.cs
+++ src/LogisticProcessor.Web/Server/Startup.cs
@@ -22,6 +22,7 @@ namespace LogisticProcessor.Web.Server
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //services.AddScoped<>;
 
             services.AddControllersWithViews();
             services.AddRazorPages();
